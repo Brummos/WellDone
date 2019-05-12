92c5ca476c3f29dba1641262c501013e2dbbece2
Bram ten Cate <agj.tencate@gmail.com>
2019-05-12 23:57:44 +0200

    #00000|WellDone|added pixel collision

--- js/game.js
+++ js/game.js
@@ -1,13 +1,9 @@
-var FPS = 60;
 const canvas = document.querySelector("#canvas_enemies");
 const ctx = canvas.getContext("2d");
-
-const width = (canvas.width)// = window.innerWidth);
-const height = (canvas.height)// = window.innerHeight);
+const width = (canvas.width)
+const height = (canvas.height)
 
 var gExpl = document.getElementById("canvas_explosions").getContext('2d');
-var gExplc = document.getElementById("canvas_explosions");
-
 var gPlanet = document.getElementById("canvas_planet").getContext('2d');
 var planet = new Planet(0, 0);
 
@@ -35,18 +31,8 @@ function Planet(x, y) {
     };
 }
 
-function render() {
-    gPlanet.clearRect(0, 0, width, height);
-    planet.render();
-}
-
-function rotatePlanet() {
-    gPlanet.translate(planet.width/2, planet.height/2);
-    gPlanet.rotate(- (Math.PI / 180) /10);
-    gPlanet.translate(-planet.width/2, -planet.height/2);
 
 
-}
 
 class nBodyProblem {
     constructor(params) {
@@ -62,27 +48,6 @@ class nBodyProblem {
 
         for (let i = 0; i < massesLen; i++) {
             const massI = this.masses[i];
-
-            if (i == 1) {
-
-
-             ///  console.log(massI.x * scale) + width /2;
-
-               // const x = (massI.x + width * 2) * scale;
-
-
-                massI.rx += massI.vx * this.dt;
-                massI.ry += massI.vy * this.dt;
-
-                var pixelData = gPlanet.getImageData(massI.rx, massI.ry, 1, 1).data;//event.offsetX, event.offsetY, 1, 1).data;
-                for (var zx = 0, n = pixelData.length; zx < n; zx += 4) {
-                    // console.log(pixelData[zx+3].toString());
-                }
-            }
-
-
-
-
 
             massI.x += massI.vx * this.dt;
             massI.y += massI.vy * this.dt;
@@ -115,27 +80,7 @@ class nBodyProblem {
             const massI = this.masses[i];
 
             for (let j = 0; j < this.masses.length; j++) {
-
-
-
                 if (i !== j) {
-
-
-
-
-
-                    // var pixelData = gPlanet.getImageData(massJ.x, massJ.y, 1, 1).data;//event.offsetX, event.offsetY, 1, 1).data;
-                    // for (var z = 0, n = pixelData.length; z < n; z += 4) {
-                    //     console.log(pixelData[z+3].toString());
-                    // }
-
-
-
-
-                    ////nice
-                    // this.masses.splice(j, 1);
-                    // break;
-
                     const massJ = this.masses[j];
 
                     if (massJ.radius == 4) {
@@ -151,23 +96,10 @@ class nBodyProblem {
                                 var explosion = new Explosion(indexX, indexY);
                                 explosion.render();
                                 this.masses.splice(j, 1);
-
-
-
                                 break;
                             }
                         }
                     }
-                    //console.log(massJ.x*100);
-
-//massJ.x massJ.y
-//                     var pixelData = gPlanet.getImageData(500, 500, 1, 1).data;//event.offsetX, event.offsetY, 1, 1).data;
-//                     for (var z = 0, n = pixelData.length; z < n; z += 4) {
-//                         console.log(pixelData[z+3].toString());
-//                     }
-
-
-
 
                     const dx = massJ.x - massI.x;
                     const dy = massJ.y - massI.y;
@@ -182,11 +114,6 @@ class nBodyProblem {
                     ax += dx * f;
                     ay += dy * f;
                     az += dz * f;
-
-
-                    //if (collision(massI, massJ)) {
-                    //massJ.destroy(j);
-                    //}
                 }
             }
 
@@ -199,17 +126,13 @@ class nBodyProblem {
     }
 }
 
-/*
- * Inputs for our nBodyProblem
- */
-
 const g = 20;//39.5; //gravity
 const dt = 0.008; //0.005 years is equal to 1.825 days //speed
 const softeningConstant = 0.15;
 
 const masses = [
     {
-        name: "", //We use solar masses as the unit of mass, so the mass of the Sun is exactly 1
+        name: "",
         m: 2,
         x: -1.50324727873647e-6,
         y: -3.93762725944737e-6,
@@ -217,8 +140,6 @@ const masses = [
         vx: 3.1669325898331e-5,
         vy: -6.85489559263319e-6,
         vz: -7.90076642683254e-7,
-        rx: 0,
-        ry: 0,
         radius: 300
     }
 ];
@@ -233,11 +154,8 @@ const innerSolarSystem = new nBodyProblem({
 class Manifestation {
     constructor(ctx, trailLength, radius) {
         this.ctx = ctx;
-
         this.trailLength = trailLength;
-
         this.radius = radius;
-
         this.positions = [];
     }
 
@@ -284,7 +202,7 @@ class Manifestation {
     }
 }
 
-const scale = 300; //300
+const scale = 300;
 const trailLength = 35;
 
 const populateManifestations = masses => {
@@ -293,7 +211,7 @@ const populateManifestations = masses => {
             (mass["manifestation"] = new Manifestation(
                 ctx,
                 trailLength,
-                mass.radius//radius
+                mass.radius
             ))
     );
 };
@@ -306,19 +224,11 @@ let currentMouseX = 0;
 let currentMouseY = 0;
 let dragging = false;
 
-canvas.addEventListener(
+const massesList = document.querySelector("#masses-list");
+
+addEventListener(
     "mousedown",
     e => {
-
-        // console.log(e.clientX)
-        // console.log(e.clientY)
-
-        // var pixelData = gPlanet.getImageData(e.clientX, e.clientY, 1, 1).data;//event.offsetX, event.offsetY, 1, 1).data;
-        // for (var z = 0, n = pixelData.length; z < n; z += 4) {
-        //     console.log(pixelData[z+3].toString());
-        // }
-
-
         mousePressX = e.clientX;
         mousePressY = e.clientY;
         dragging = true;
@@ -326,7 +236,7 @@ canvas.addEventListener(
     false
 );
 
-canvas.addEventListener(
+addEventListener(
     "mousemove",
     e => {
         currentMouseX = e.clientX;
@@ -335,9 +245,7 @@ canvas.addEventListener(
     false
 );
 
-const massesList = document.querySelector("#masses-list");
-
-canvas.addEventListener(
+addEventListener(
     "mouseup",
     e => {
         const x = (mousePressX - width / 2) / scale;
@@ -347,8 +255,6 @@ canvas.addEventListener(
         const vx = (e.clientX - mousePressX) / 35;
         const vy = (e.clientY - mousePressY) / 35;
         const vz = 0;
-        const rx = mousePressX;
-        const ry = mousePressY;
         const radius = 4;
 
         innerSolarSystem.masses.push({
@@ -359,8 +265,6 @@ canvas.addEventListener(
             vx,
             vy,
             vz,
-            rx,
-            ry,
             radius,
             manifestation: new Manifestation(ctx, trailLength, radius)
         });
@@ -369,6 +273,18 @@ canvas.addEventListener(
     },
     false
 );
+
+
+function rotatePlanet() {
+    gPlanet.translate(planet.width/2, planet.height/2);
+    gPlanet.rotate(- (Math.PI / 180) /10);
+    gPlanet.translate(-planet.width/2, -planet.height/2);
+}
+
+function render() {
+    gPlanet.clearRect(0, 0, width, height);
+    planet.render();
+}
 
 const animate = () => {
     render();
